6ecfc60ddfcbde53f1c55df39077bfb905b3c791
Loránd Biró <lorand.biro@outlook.com>
2024-06-21 23:00:41 +0200

    Add open zone and short circuit to Prometheus

--- src/IrrigationController/Controllers/MetricsController.cs
+++ src/IrrigationController/Controllers/MetricsController.cs
@@ -1,4 +1,5 @@
-﻿using IrrigationController.Core.Services;
+﻿using IrrigationController.Core.Domain;
+using IrrigationController.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 
@@ -6,15 +7,20 @@ namespace IrrigationController.Controllers;
 
 [Route("metrics")]
 [ApiController]
-public class MetricsController(Config config, SoilMoistureEstimator soilMoistureEstimator) : ControllerBase
+public class MetricsController(Config config, SoilMoistureEstimator soilMoistureEstimator, ProgramController programController, IZoneRepository zoneRepository) : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
     {
-        StringBuilder sb = new StringBuilder();
-        foreach ((string name, double soilMoisture) in config.Zones.Select((zone, i) => (zone.Name, soilMoistureEstimator.Estimate(i, DateTime.UtcNow))))
+        StringBuilder sb = new();
+        for (int i = 0; i < config.Zones.Count; i++)
         {
-            sb.AppendLine($"soil_moisture{{zone=\"{name}\"}} {soilMoisture}");
+            ZoneInfo zoneInfo = config.Zones[i];
+            Zone? zone = zoneRepository.Get(i);
+            double soilMoisture = soilMoistureEstimator.Estimate(i, DateTime.UtcNow);
+            sb.AppendLine($"soil_moisture{{zone=\"{zoneInfo.Name}\"}} {soilMoisture}");
+            sb.AppendLine($"zone_open{{zone=\"{zoneInfo.Name}\"}} {(programController.CurrentZone?.ZoneId == i ? "1" : "0")}");
+            sb.AppendLine($"zone_short_circuit{{zone=\"{zoneInfo.Name}\"}} {(zone?.IsDefective == true ? "1" : "0")}");
         }
 
         return this.Content(sb.ToString(), "text/plain", Encoding.UTF8);
