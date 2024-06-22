7b0296a4217a0aa412c8ab40c3867deebd606cb0
Loránd Biró <lorand.biro@outlook.com>
2024-06-22 11:00:43 +0200

    Show logs at the end of publish

--- publish.sh
+++ publish.sh
@@ -7,3 +7,5 @@ sudo systemctl stop irrigation-controller
 sudo rm -rf /usr/local/irrigation-controller
 sudo cp -r src/IrrigationController/bin/Release/net8.0/publish/. /usr/local/irrigation-controller
 sudo systemctl start irrigation-controller
+
+journalctl -u irrigation-controller -f
